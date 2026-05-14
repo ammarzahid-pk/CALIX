198f6673d02c160e6092f3bb81b7339b6d9a933f
Ammar <ammarzahid.pk@gmail.com>
2026-05-14 15:42:27 +0500

    added pwa support

--- vite.config.ts
+++ vite.config.ts
@@ -4,13 +4,45 @@ import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 import { viteSingleFile } from "vite-plugin-singlefile";
+import { VitePWA } from "vite-plugin-pwa";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss(), viteSingleFile()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    viteSingleFile(),
+
+    VitePWA({
+      registerType: "autoUpdate",
+
+      manifest: {
+        name: "CALIX",
+        short_name: "CALIX",
+        description: "Master Your Movement",
+        theme_color: "#000000",
+        background_color: "#000000",
+        display: "standalone",
+
+        icons: [
+          {
+            src: "/icon-192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "/icon-512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+    }),
+  ],
+
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "src"),
